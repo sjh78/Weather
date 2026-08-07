510476630584decd37f12e3cb3e2b94152875139
sjh78 <stevenjhamm@yahoo.ca>
2026-08-07 10:49:50 -0500

    Fetch and print current weather data from URL

--- update_weather.py
+++ update_weather.py
@@ -1,1 +1,13 @@
-print("Hello from GitHub Actions")
+import requests
+2
+ 
+3
+url = "https://www.gov.mb.ca/conservation_fire/Wx-Hour/currentwx.txt"
+4
+ 
+5
+response = requests.get(url)
+6
+ 
+7
+print(response.text)
