a157aa5c5530502d112a93862ac92f4c7e817df4
sjh78 <stevenjhamm@yahoo.ca>
2026-08-19 11:35:11 -0500

    Add additional print statement for debugging

--- ND_coords_populate.py
+++ ND_coords_populate.py
@@ -53,6 +53,7 @@ def get_station_info_url(station_name):
 
         soup = BeautifulSoup(r.text, "html.parser")
 
+        print("Trying URL:", url)
         print(r.text[:500])
 
         for a in soup.find_all("a", href=True):
